b702a296e91aae67af6e88cc8e958f2473ffe886
Marcos Lima <marcos.j.s.lima@gmail.com>
2019-10-21 15:40:08 -0300

    Linker now is imported from pylint.pyreverse.inspector project_from_files now is imported from pylint.pyreverse.main. register_options_provider commented out for while due to an error it raises. To be better analyzed .

--- pyplantuml/adapter.py
+++ pyplantuml/adapter.py
@@ -4,11 +4,13 @@ import sys
 from logilab.common.configuration import ConfigurationMixIn
 
 from astroid.manager import AstroidManager
-from astroid.inspector import Linker
+from pylint.pyreverse.inspector import Linker
+
 
 from pylint.pyreverse.main import Run
 from pylint.pyreverse.diadefslib import DiadefsHandler
 from pylint.pyreverse.utils import insert_default_options
+from pylint.pyreverse.main import project_from_files
 
 
 # TODO: Get package arg and add to PATH as well.
@@ -21,7 +23,7 @@ class PyreverseAdapter(Run):
         ConfigurationMixIn.__init__(self, usage=__doc__)
         insert_default_options()
         self.manager = AstroidManager()
-        self.register_options_provider(self.manager)
+        # self.register_options_provider(self.manager)
         self.args = self.load_command_line_configuration()
 
     def run(self):
@@ -32,7 +34,7 @@ class PyreverseAdapter(Run):
         # dependencies to local modules even if cwd is not in the PYTHONPATH.
         sys.path.insert(0, os.getcwd())
         try:
-            project = self.manager.project_from_files(self.args)
+            project = project_from_files(self.args)
             linker = Linker(project, tag=True)
             handler = DiadefsHandler(self.config)
             diadefs = handler.get_diadefs(project, linker)
